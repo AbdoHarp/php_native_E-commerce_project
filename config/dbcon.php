f2ee8a2e64a5369ff8c6bb73d83603e08563ab49
abdo harp <abdoharp234@gmail.com>
2023-03-08 07:22:03 +0200

    Config Folder with conected database

--- config/dbcon.php
+++ config/dbcon.php
@@ -10,6 +10,4 @@ $con = mysqli_connect($host, $username, $password, $dbname);
 // Check database conection
 if (!$con) {
     die("Connection Failed:" . mysqli_connect_errno());
-} else {
-    echo "Connected sucessfully";
 }
